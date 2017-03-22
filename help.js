4b78c9c9c6b6c49b803939c56d9ec48ec771d6e5
Marc Sebes <marcsebes@Marcs-MacBook-Air.local>
2017-03-21 21:51:17 -0400

    Added Help

---
+++ help.js
@@ -0,0 +1,27 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Cupcake v2 Viewer</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<link rel="stylesheet" href="assets/css/flat/zebra_dialog.css" type="text/css">
+		<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
+		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+		<!-- <script src="https://apis.google.com/js/platform.js" async defer></script> -->
+	</head>
+	<body>
+		<!-- Wrapper -->
+			<div id="wrapper">
+				<!-- Main -->
+					<div id="main">
+						<div class="inner">
+                        To use cupcake, you need to add a few parametesr to the URL:
+                        <br/>
+                        <ul>
+                        <li>user</li>
+                        <li>date/li>
+                        <li>acccount/li>
+                        </ul>
+                        
